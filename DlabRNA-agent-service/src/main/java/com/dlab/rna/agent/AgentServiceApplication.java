9381eb2bb74885b3a5ea2d1ffdaeca8f9ace9841
chen <1571364101@qq.com>
2025-10-18 21:10:24 +0800

    feat(auth): implement login, registration, and frontend

---
+++ DlabRNA-agent-service/src/main/java/com/dlab/rna/agent/AgentServiceApplication.java
@@ -0,0 +1,16 @@
+﻿package com.dlab.rna.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Agent服务启动类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AgentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentServiceApplication.class, args);
+    }
+}
